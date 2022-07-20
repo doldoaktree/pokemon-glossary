8a4900e1c1d0d7690223921b25b2c2d03742bcde
doldoaktree <doldoaktree@gmail.com>
2022-07-20 17:24:21 +1000

    not much has changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import {Container, Box, Stack} from '@mui/material';
 import Sidebar from '../static/components/sidebar';
@@ -11,42 +12,42 @@ import Navbar from '../static/components/navbar';
 import dataOfCharacters from '../static/data';
 
 
+
 function MediaCard(props: TypeOfValues) {
 
   const {image, name, description} = props;
   return (
-      <Card sx={{ maxWidth: 220, 
-                  bgcolor: 'background.paper',
-                  boxShadow:3,
-                  borderRadius:2,
-                  p:2,
-                  minWidth:85,
-              }}>
-        <CardMedia 
-          sx={{width:96}}
-          component="img"
-          height='96'
-          image={image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-    </Card>
+    <Card sx={{ maxWidth: 220, 
+                boxShadow:3,
+                borderRadius:2,
+                p:2,
+                minWidth:85,
+            }}>
+      <CardMedia 
+        sx={{width:96}}
+        component="img"
+        height='96'
+        image={image}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>   
   );
 }
 
 export interface TypeOfValues {
-  characterId: number,
+  characterId?: number,
   name: string,
   image: string,
   description: string
@@ -57,9 +58,10 @@ type Props = { items: TypeOfValues[] };
 function MapChars(props: Props) {
   const { items } = props;
   const listOfCharacters = items.map((value, index) => {
+    const {description, name, image} = value;
     return (
-      <div key={index}>
-        <MediaCard description={value.description} characterId={value.characterId} name={value.name} image={value.image} />
+      <div key={index}> 
+          <MediaCard description={description} name={name} image={image} />
       </div>
     );
   });
@@ -71,16 +73,16 @@ function MapChars(props: Props) {
 
 function App() {
   return (
-  <Container>
-    <Box bgcolor='skyblue'>
-      <Stack direction="column">
-        <Sidebar />
-        <Navbar />
-        <h1>Pokemon</h1>
-        <MapChars items={dataOfCharacters} />
-      </Stack>
-    </Box>
-  </Container>
+    <Container>
+      <Sidebar />
+      <Navbar />
+      <h1>Pokemon</h1>
+      <Box bgcolor='skyblue'>
+        <MapChars items={dataOfCharacters}/>
+       <Button variant='contained' color='primary'>I'm Benjamin</Button>
+       <Button variant='contained' color='secondary'>I'm Benjamin</Button>
+      </Box>
+    </Container>
   );
 }
 export default App;
